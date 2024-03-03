5e57de0669674d85ebe9ffaa6cd8273c22febd0e
kong68 <74444856+kong68@users.noreply.github.com>
2024-03-03 22:22:25 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = ""
+    for i in range(1,n+1):
+        if i%2 !=0: #홀수 일 경우
+            answer += "수"
+            
+            
+        else:
+            answer += "박"
+    
+    return answer
